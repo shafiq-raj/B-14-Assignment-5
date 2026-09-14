90dbc3a5957c01b83f864f306a85a9af48955dc5
shafiq-raj <shafiq.tsc@gmail.com>
2026-09-14 23:25:56 +0600

    hero completed

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import banner from "../assets/banner-stack.png"
 
 const Banner = () => {
   return (
-    <div className="mx-auto max-w-5xl my-24">
+    
+    <div className="mx-auto max-w-5xl my-2 flex justify-between items-center">
+        <div>
       <h1 className="font-inter text-5xl font-bold leading-tight ">
         Build Your Ideal
         <br />
@@ -10,16 +13,19 @@ const Banner = () => {
           Development Stack
         </span>
       </h1>
-      <div>
+      
         <p className="font-plus jakarta-sans text-slate-600">
           Explore frontend, backend, database, and tooling options,<br /> compare them
           side by side, and put together the stack that fits your <br /> next project.
         </p>
-      </div>
-      <div className="gap-3">
+     
+      
         <button className="font-inter my-4 rounded-lg bg-gradient-to-r from-orange-400 to-red-500 px-5 py-2 text-white">Explore Technologies</button>
         <button className="font-inter my-4 rounded-lg text-black border border-slate-400 px-12 py-2 ml-3">Learn More</button>
-       
+       </div>
+    
+      <div>
+        <img src={banner} alt="" className="w-[380px] h-[380px]" />
       </div>
     </div>
   );
